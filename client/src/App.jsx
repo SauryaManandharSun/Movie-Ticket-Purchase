70599a412a287f90192a20fde87c1e52c044eb66
Saurya Manandhar <sunsauryaman.com>
2026-02-15 16:51:06 +0545

    Components Building

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,33 @@
 import React from 'react'
+import { Route, Routes, useLocation } from 'react-router-dom'
+import Home from './pages/Home'
+import MovieDetails from './pages/MovieDetails'
+import SeatLayout from './pages/SeatLayout'
+import MyBookings from './pages/MyBookings'
+import Favorite from './pages/Favorite'
+import Navbar from './components/Navbar'
+import Footer from './components/Footer'
+import Movies from './pages/Movies'
+import { Toaster } from 'react-hot-toast'
 
 export default function App() {
+
+  const isAdminRoute = useLocation().pathname.startsWith('/admin')
   return (
-    <div>App</div>
+    <>
+      <Toaster />
+      {!isAdminRoute && <Navbar />}
+
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/movies' element={<Movies />} />
+        <Route path='/movies/:id' element={<MovieDetails />} />
+        <Route path='/movies/:id/:date' element={<SeatLayout />} />
+        <Route path='/my-bookings' element={<MyBookings />} />
+        <Route path='/favorite' element={<Favorite />} />
+      </Routes>
+      {!isAdminRoute && <Footer />}
+
+    </>
   )
 }
