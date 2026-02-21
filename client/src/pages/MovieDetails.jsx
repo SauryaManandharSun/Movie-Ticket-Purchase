87f5e1d042c380be65da09e3497031bb78c614f9
Saurya Manandhar <sunsauryaman.com>
2026-02-21 15:33:09 +0545

    Loading component and added more movies at the end

--- client/src/pages/MovieDetails.jsx
+++ client/src/pages/MovieDetails.jsx
@@ -1,20 +1,28 @@
 import { useEffect, useState } from 'react';
-import { useParams } from 'react-router-dom';
+import { useNavigate, useParams } from 'react-router-dom';
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets';
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react';
 import BlurCircle from '../components/BlurCircle';
 import timeFormat from '../lib/timeFormat';
 import DateSelect from '../components/DateSelect';
+import MovieCard from '../components/MovieCard';
+import Loading from '../components/Loading';
 
 const MovieDetails = () => {
+
+  const navigate = useNavigate()
   const { id } = useParams();
   const [show, setShow] = useState(null);
   const getShow = async () => {
     const show = dummyShowsData.find(show => show._id === id)
-    setShow({
-      movie: show,
-      dateTime: dummyDateTimeData
-    })
+    if (show) {
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+
+    }
+
   }
   useEffect(() => {
     getShow()
@@ -54,9 +62,9 @@ const MovieDetails = () => {
       </p>
       <div className='overflow-x-auto no-scrollbar mt-8 pd-4'>
         <div className='flex items-center gap-4 w-max px-4'>
-          {show.movie.casts.slice(0,12).map((cast,index)=>(
+          {show.movie.casts.slice(0, 12).map((cast, index) => (
             <div key={index} className='flex flex-col items-center text-center'>
-              <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover'/>
+              <img src={cast.profile_path} alt="" className='rounded-full h-20 md:h-20 aspect-square object-cover' />
               <p className='font-medium text-xs mt-3'>
                 {cast.name}
               </p>
@@ -67,12 +75,25 @@ const MovieDetails = () => {
         </div>
 
       </div>
-          <DateSelect datetime={show.dateTime} id={id}/>
+      <DateSelect datetime={show.dateTime} id={id} />
+      <p className='text-lg font-medium mt-20 mb-8'>You May Also Like</p>
+      <div className='flex flex-wrap max-sm:justify-cneter gap-8'>
+        {
+          dummyShowsData.slice(0, 4).map((movie, index) => (
+            <MovieCard key={index} movie={movie} />
+          ))
+        }
+
+      </div>
+      <div className='flex justify-center mt-20'>
+        <button onClick={() => { navigate('/movies'); scrollTo(0, 0) }} className='px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer'>Show More
+
+        </button>
+
+      </div>
     </div>
   ) :
-    <div>
-      Loading...
-    </div>
+    <Loading/>
 }
 
 export default MovieDetails
