bda57c126678f785931766316bb0a6d0b89b0a6d
Saurya Manandhar <sunsauryaman@gmail.com>
2026-02-24 18:51:21 +0545

    backend installation and tested the port

---
+++ server/server.js
@@ -0,0 +1,16 @@
+import express from 'express';
+import cors from 'cors';
+import 'dotenv/config';
+
+const app = express();
+const port = 3000;
+
+//MiddleWare
+app.use(express.json);
+app.use(cors());
+
+// Api routes
+app.get('/', (req,res)=> res.send('Server is live'));
+
+// Connect it to the datebase
+app.listen(port, ()=>console.log(`Server is running at the port number ${port}`));
